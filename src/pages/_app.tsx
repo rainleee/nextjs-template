952925b47846939a694af604648d9f1df8d7eeaa
rainleee <rainlee4401@gmail.com>
2023-07-24 11:21:43 +0900

    feat(#13): apollo client provider 세팅 및 codegen 세팅 Closes #13

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { ApolloProvider } from "@apollo/client";
+import type { AppProps } from "next/app";
+
+import { initializeApollo } from "@/gql/apollo";
+
+const apolloClient = initializeApollo();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={apolloClient}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
